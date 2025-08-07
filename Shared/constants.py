eaa6751516aafcac511222166ab0aca9136397de
Minseo Kim <charlottekimms@gmail.com>
2025-08-07 13:58:38 -0700

    Added the server IP address

--- Shared/constants.py
+++ Shared/constants.py
@@ -1,4 +1,4 @@
 ## Define constants used across the project
 SERVER_PORT = 12345  # Default port for the server
-SERVER_ADDRESS = ""  # Default address for the server
+SERVER_ADDRESS = "100.121.224.90"  # Default address for the server
 GEM_RADIUS = 20      # Default size for gems
